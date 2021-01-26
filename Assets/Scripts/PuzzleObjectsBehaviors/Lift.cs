93af026fbc0f43ac87b162b6b969c3f5d80f1f8a
SteshenkoSv <bigslav98@gmail.com>
2021-01-26 09:47:44 +0300

    Moving platform fix, lever for moving platform, scene clean up

---
+++ Assets/Scripts/PuzzleObjectsBehaviors/Lift.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    [SerializeField] private Vector3 _startPos;
+    [SerializeField] private Vector3 _endPos;
+
+    private bool _movedToEnd;
+
+    public float travelDuration = 2f;
+    void Start()
+    {
+        transform.position = _startPos;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float counter = 0f;
+        while (counter < travelDuration)
+        {
+            transform.position = Vector3.Lerp(_startPos, _endPos, counter / travelDuration);
+            counter += Time.deltaTime;
+        }
+        transform.position = _endPos;
+    }
+}
--- Assets/Scripts/PuzzleObjectsBehaviors/ObjectActivatorBehaviors/PlatformLever.cs
+++ Assets/Scripts/PuzzleObjectsBehaviors/ObjectActivatorBehaviors/PlatformLever.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformMovingTrigger : MonoBehaviour
+public class PlatformLever : MonoBehaviour
 {
     public PlatformMoving movingPlatform;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if ((other.gameObject.layer == 8 || other.gameObject.layer == 9))
+        if ((other.gameObject.layer == 8 || other.gameObject.layer == 9) && Input.GetKeyDown(KeyCode.E))
         {
             movingPlatform.NextPlatform();
         }
