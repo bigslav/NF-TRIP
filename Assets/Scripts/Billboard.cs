7c2b016c5027d79a90d035374f89fda99413bb2e
SteshenkoSv <bigslav98@gmail.com>
2021-02-02 12:29:58 +0300

    2d object positioning script added

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+
+
+    void LateUpdate()
+    {
+        transform.LookAt(_mainCamera.transform);
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+}
