b1efcc99141d129fbdfa0132936a2524dec50277
Armund <erm_alex@list.ru>
2021-02-09 17:53:06 +0300

    minor update

---
+++ Assets/MovePlatf.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatf : MonoBehaviour
+{
+	float distance;
+	bool directionRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+		distance = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (directionRight)
+		{
+			transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+			distance += Time.deltaTime;
+			if (distance > 3)
+			{
+				directionRight = false;
+			}
+		} else
+		{
+			transform.position -= new Vector3(1, 0, 0) * Time.deltaTime;
+			distance -= Time.deltaTime;
+			if (distance < 0)
+			{
+				directionRight = true;
+			}
+		}
+		
+    }
+}
---
+++ Assets/MovePlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter(Collision coll)
+	{
+		coll.transform.parent = transform;
+		Debug.Log("yes");
+	}
+
+	void OnCollisionExit(Collision coll)
+	{
+		coll.transform.parent = null;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		other.transform.parent = transform;
+		Debug.Log("yes");		
+	}
+
+	void OnTriggerExit(Collider coll)
+	{
+		coll.transform.parent = null;
+	}
+}
