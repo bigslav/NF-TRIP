05841024592ea0fccb29728632d15dbb4ee200dd
SteshenkoSv <bigslav98@gmail.com>
2021-02-13 17:21:37 +0300

    Ermakov puzzle build v1

---
+++ Assets/Scripts/PuzzleObjectsBehaviors/BridgeActivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeActivator : MonoBehaviour
+{
+    [SerializeField] private BridgeJointRotation _bridge;
+    [SerializeField] private LayerMask _whoCanInteract;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_whoCanInteract == (_whoCanInteract | (1 << other.gameObject.layer)))
+        {
+            if (!_bridge._beingLifted)
+            {
+                _bridge._beingLifted = true;
+                StartCoroutine(_bridge.Activate());
+            }
+        }
+    }
+}
