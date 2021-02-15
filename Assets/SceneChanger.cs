aaa77f22b984a59386eef16cc8adc6f60dec9c70
SteshenkoSv <bigslav98@gmail.com>
2021-02-15 22:11:29 +0300

    NikitaG puzzles build

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void PrevScene()
+    {
+        Scene active = SceneManager.GetActiveScene();
+
+        if (active.buildIndex > 0)
+        {
+            SceneManager.LoadScene(active.buildIndex - 1);
+        }
+        else if(active.buildIndex == 0)
+        {
+            SceneManager.LoadScene(5);
+        }
+    }
+
+    public void NextScene()
+    {
+        Scene active = SceneManager.GetActiveScene();
+
+        if (active.buildIndex < 5)
+        {
+            SceneManager.LoadScene(active.buildIndex + 1);
+        }
+        else if (active.buildIndex == 5)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
