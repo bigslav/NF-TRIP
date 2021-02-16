207322f770fd75c4eb8da992934363cd1da05f34
Aleksander Mort <aleksandermort@gmail.com>
2021-02-16 12:47:40 +0300

    Добавлен тестовый проект (golem_anim) с анимацией голема

---
+++ golem_anim/Assets/New_Game/InputDevices/KeyboardInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace new_game
+{
+    public class KeyboardInput : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                VirtualInputManager.Instance.MoveRight = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.MoveRight = false;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                VirtualInputManager.Instance.MoveLeft = true;
+            }
+            else
+            {
+                VirtualInputManager.Instance.MoveLeft = false;
+            }
+        }
+    }
+}
+
---
+++ golem_anim/Assets/New_Game/Managers/VirtualInputManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace new_game
+{
+    public class VirtualInputManager : Singleton<VirtualInputManager>
+    {
+        public bool MoveRight;
+        public bool MoveLeft;
+    }
+}
