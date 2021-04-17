1537f44a2c63c06d240e20aad496030f5f5acd75
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-04-17 11:01:50 +0300

    Level 1: add transition to Level 2

---
+++ Assets/Scripts/Gameplay/NextLevelTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    public string Level;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(Level);
+    }
+}
