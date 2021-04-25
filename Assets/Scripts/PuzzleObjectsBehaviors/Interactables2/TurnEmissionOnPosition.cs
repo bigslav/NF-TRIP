b4c66f4e9502f5dbda408e69e0b5ee4e8ff9f88d
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-04-25 23:02:32 +0300

    Level 2: puzzle 1 platforms and activators

---
+++ Assets/Scripts/PuzzleObjectsBehaviors/Interactables2/TurnEmissionOnPosition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TurnEmissionOnPosition : MonoBehaviour
+{
+    public MeshRenderer targetMeshRenderer;
+    public Transform turnEmissionOnPos;
+    public Transform turnEmissionOffPos;
+
+    private Material _mat;
+    private Color _col;
+
+    private void Start()
+    {
+        _mat = targetMeshRenderer.material;
+        _col = _mat.GetColor("_EmissionColor");
+    }
+
+    private void Update()
+    {
+        if (transform.position == turnEmissionOnPos.position)
+        {
+            _mat.EnableKeyword("_EMISSION");
+            _mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            _mat.SetColor("_EmissionColor", _col);
+        }
+        else if (transform.position == turnEmissionOffPos.position) 
+        {
+            _mat.DisableKeyword("_EMISSION");
+            _mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
+            _mat.SetColor("_EmissionColor", Color.black);
+        }
+    }
+}
