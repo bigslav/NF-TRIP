dd4f47babf0f7ae2816188fa3f62197b3a9e2efe
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-04-28 23:33:16 +0300

    Level 2: transition art initial

---
+++ Assets/Scripts/Test/ChangeLayerOnRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLayerOnRotation : MonoBehaviour
+{
+    public Vector3 rotation;
+
+    private void Start()
+    {
+        if (transform.eulerAngles == rotation)
+        {
+            gameObject.layer = 10;
+        }
+        else
+        {
+            gameObject.layer = 0;
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(transform.eulerAngles + " | " + rotation);
+        if (Vector3.SqrMagnitude(transform.eulerAngles - rotation) < 0.0001f)
+        {
+            gameObject.layer = 10;
+            Debug.Log("Layer Upd");
+        }
+        else
+        {
+            gameObject.layer = 0;
+        }
+    }
+
+}
