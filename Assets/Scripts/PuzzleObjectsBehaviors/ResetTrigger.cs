4c1b801061a64b2298a10e9701a9292233c3bca0
Nikita_G <nikita.gorlyak@yandex.ru>
2021-05-05 20:58:56 +0300

    Logs and boats with art

--- Assets/Scripts/PuzzleObjectsBehaviors/ResetTrigger.cs
+++ Assets/Scripts/PuzzleObjectsBehaviors/ResetTrigger.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ResetTrigger : MonoBehaviour
 {
+    public bool totalReset = false;
     private void OnTriggerEnter(Collider collision)
     {
-        Debug.Log("A");
         if (collision.gameObject.layer == 8 || collision.gameObject.layer == 9)
         {
-            collision.gameObject.GetComponent<InputHandler>().Spawn();
+            if (totalReset)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            else
+                collision.gameObject.GetComponent<InputHandler>().Spawn();
         }
     }
 }
