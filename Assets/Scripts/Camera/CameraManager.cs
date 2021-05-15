3875414f387aa5f5ea1fd5e2b16b86f16c3e6c06
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-05-15 21:17:21 +0300

    Build 0.1.0.8: quarter-final

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("CameraManager");
+
+        if (objs.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
