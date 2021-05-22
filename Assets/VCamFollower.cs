3924b2670e5161cf6fc00300748174781036bbe6
SteshenkoSv <sviatoslav.steshenko@gmail.com>
2021-05-22 21:40:06 +0300

    WIP

---
+++ Assets/VCamFollower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class VCamFollower : MonoBehaviour
+{
+    public string objectToFollowTag;
+
+    private GameObject objectToFollow;
+    private CinemachineVirtualCamera VCam;
+    
+    private void Start()
+    {
+        VCam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    void Update()
+    {
+        if (objectToFollow == null) 
+        {
+            objectToFollow = GameObject.FindGameObjectWithTag(objectToFollowTag);
+            VCam.Follow = objectToFollow.transform;
+        }
+    }
+}
