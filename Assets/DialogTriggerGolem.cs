511f1d0c089d4d29d75716df163956651aaeef84
Maxim Panchenko <32279426+fireokay@users.noreply.github.com>
2021-05-25 04:51:13 +1100

    Audio: Level 2 - Dialogues

---
+++ Assets/DialogTriggerGolem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTriggerGolem : MonoBehaviour
+{
+	private Dialog dialog;
+	private bool isMushroomInside = false;
+	private bool isGolemInside = false;
+	private bool isDialogPlayed = false;
+
+	private void Start()
+	{
+		dialog = GetComponent<Dialog>();
+	}
+
+	private void PlayDialog()
+	{
+		DialogPlayer.instance.PlayDialog(dialog);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Golem"))
+		{
+			isGolemInside = true;
+		}
+
+		if (isGolemInside && !isDialogPlayed)
+		{
+			isDialogPlayed = true;
+			PlayDialog();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Golem"))
+		{
+			isGolemInside = false;
+		}
+	}
+}
---
+++ Assets/DialogTriggerMushroom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTriggerMushroom : MonoBehaviour
+{
+	private Dialog dialog;
+	private bool isMushroomInside = false;
+	private bool isGolemInside = false;
+	private bool isDialogPlayed = false;
+
+	private void Start()
+	{
+		dialog = GetComponent<Dialog>();
+	}
+
+	private void PlayDialog()
+	{
+		DialogPlayer.instance.PlayDialog(dialog);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Mushroom"))
+		{
+			isMushroomInside = true;
+		}
+
+		if (isMushroomInside && !isDialogPlayed)
+		{
+			isDialogPlayed = true;
+			PlayDialog();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Mushroom"))
+		{
+			isMushroomInside = false;
+		}
+	}
+}
